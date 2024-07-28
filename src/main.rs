0b7f099ff3d5a94a38e66a626378c0706ab890bc
zjp <jiping_zhou@foxmail.com>
2024-07-28 16:19:55 +0800

    test: check if stdout capture is ok on ci/docker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let output = std::process::Command::new("echo")
+        .args(["hello", " world"])
+        .output()
+        .unwrap();
+    println!(
+        "success={} stdout={:?} stderr={:?}",
+        output.status.success(),
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr),
+    );
 }
