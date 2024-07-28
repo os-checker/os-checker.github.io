f71a947ec70e275eefe4156a7f731bc33f8638fc
zjp <jiping_zhou@foxmail.com>
2024-07-28 16:22:31 +0800

    sh: don't run os-checker

--- run.sh
+++ run.sh
@@ -4,25 +4,25 @@
 cargo r --release
 
 # install latest os-checker
-echo "DEBUG=$DEBUG"
-if [[ "$DEBUG" == 1 ]]; then
-  echo 'compile os-checker from github source'
-  git clone https://github.com/os-checker/os-checker.git
-  cargo install --path os-checker
-  export RUST_LOG=debug
-else
-  echo 'download os-checker from github release'
-  curl -s https://api.github.com/repos/os-checker/os-checker/releases/latest |
-    grep browser_download_url |
-    grep os-checker-installer.sh |
-    cut -d '"' -f 4 |
-    wget -qi -
-  echo 'os-checker-installer.sh is downloaded'
-  sh os-checker-installer.sh
-fi
-os-checker --help
-
-# run os-checker for repos.yaml
-mkdir check
-cd check
-os-checker --config ../repos.yaml
+# echo "DEBUG=$DEBUG"
+# if [[ "$DEBUG" == 1 ]]; then
+#   echo 'compile os-checker from github source'
+#   git clone https://github.com/os-checker/os-checker.git
+#   cargo install --path os-checker
+#   export RUST_LOG=debug
+# else
+#   echo 'download os-checker from github release'
+#   curl -s https://api.github.com/repos/os-checker/os-checker/releases/latest |
+#     grep browser_download_url |
+#     grep os-checker-installer.sh |
+#     cut -d '"' -f 4 |
+#     wget -qi -
+#   echo 'os-checker-installer.sh is downloaded'
+#   sh os-checker-installer.sh
+# fi
+# os-checker --help
+#
+# # run os-checker for repos.yaml
+# mkdir check
+# cd check
+# os-checker --config ../repos.yaml
