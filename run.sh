68255dca391e9c42d9d92f1d3c87712f480a7343
zjp <jiping_zhou@foxmail.com>
2024-07-28 14:32:16 +0800

    ci: use DEBUG to compile os-checker from source

--- run.sh
+++ run.sh
@@ -4,13 +4,21 @@
 cargo r --release
 
 # install latest os-checker
-curl -s https://api.github.com/repos/os-checker/os-checker/releases/latest |
-  grep browser_download_url |
-  grep os-checker-installer.sh |
-  cut -d '"' -f 4 |
-  wget -qi -
-echo 'os-checker-installer.sh is downloaded'
-sh os-checker-installer.sh
+if [[ "$DEBUG" -eq 1 ]]; then
+  echo 'compile os-checker from github source'
+  git clone https://github.com/os-checker/os-checker.git
+  cargo install --path .
+  export RUST_LOG=debug
+else
+  echo 'download os-checker from github release'
+  curl -s https://api.github.com/repos/os-checker/os-checker/releases/latest |
+    grep browser_download_url |
+    grep os-checker-installer.sh |
+    cut -d '"' -f 4 |
+    wget -qi -
+  echo 'os-checker-installer.sh is downloaded'
+  sh os-checker-installer.sh
+fi
 os-checker --help
 
 # run os-checker for repos.yaml
