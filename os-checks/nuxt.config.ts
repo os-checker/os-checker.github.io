3399face9c16867ed986fe40dc2880ed64500bf7
zjp <jiping_zhou@foxmail.com>
2024-07-30 16:02:54 +0800

    ui: add nuxt content dep

--- os-checks/nuxt.config.ts
+++ os-checks/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
   app: { baseURL: '/ci/' },
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxt/content"]
 })
