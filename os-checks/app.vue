42e9269c4de2e7e9183aff85ce82e606f872631d
zjp <jiping_zhou@foxmail.com>
2024-07-30 21:51:11 +0800

    ui: TreeTable & primeicons

--- os-checks/app.vue
+++ os-checks/app.vue
@@ -1,5 +1,26 @@
+<script setup>
+import NodeService from './utils/data'
+const nodes = await NodeService.getTreeTableNodes();
+const value = ref(null);
+</script>
+
 <template>
+  <Button label="Check" icon="pi pi-check-circle" />
+  <TreeTable :value="nodes" tableStyle="min-width: 50rem">
+    <Column field="name" header="Name" expander style="width: 34%"></Column>
+    <Column field="size" header="Size" style="width: 33%"></Column>
+    <Column field="type" header="Type" style="width: 33%"></Column>
+  </TreeTable>
+
   <div>
-    hi???
+    <i class="pi pi-check" style="font-size: 1rem"></i>
+    <i class="pi pi-times" style="font-size: 1.5rem"></i>
+    <i class="pi pi-search" style="font-size: 2rem"></i>
+    <i class="pi pi-user" style="font-size: 2.5rem"></i>
+  </div>
+
+
+  <div class="card flex justify-center">
+    <Password v-model="value" toggleMask />
   </div>
 </template>
--- os-checks/nuxt.config.ts
+++ os-checks/nuxt.config.ts
@@ -1,7 +1,15 @@
+import Aura from '@primevue/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
   app: { baseURL: '/ci/' },
   devtools: { enabled: true },
-  modules: ["@nuxt/content"]
+  modules: ["@nuxt/content", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: { preset: Aura }
+    }
+  },
+  css: ["primeicons/primeicons.css"]
 })
