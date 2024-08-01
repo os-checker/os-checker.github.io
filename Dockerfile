1460a3bde9a62a006597d1449a4902c98fc905fb
zjp <jiping_zhou@foxmail.com>
2024-08-01 08:40:54 +0800

    ci: restart Dockerfile from scratch

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,30 @@
 # 使用官方 Rust 镜像作为基础镜像  
-FROM rustlang/rust:nightly AS os-checker
+# FROM rustlang/rust:nightly AS os-checker
 
 # 安装必要的软件包  
-RUN apt-get update && apt-get install -y build-essential
+# RUN apt-get update && apt-get install -y build-essential
 
-WORKDIR /check
-COPY . .
+# WORKDIR /check
+# COPY . .
 
 # 指定容器启动时运行的命令  
-RUN ./run.sh
+# RUN ./run.sh
 
-# 构建静态网页
+# node:lts 包含完整的 debian 环境
 FROM node:lts
+# 下载最新的 nightly Rust 工具链
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y && \
+    rustup toolchain install nightly --allow-downgrade --profile minimal --component rustfmt,clippy,miri
+WORKDIR /
+CMD wget https://raw.githubusercontent.com/os-checker/ci/main/run.sh && ./run.sh
+    
 # 把 os-checks 目录的内容展平到 /os-checks 下
 # 注意：这里不能指定为 /check，貌似因为它已经是挂载卷，如果现在复制文件过去，容器运行后该 
 #       /check 目录为空（因为 github action runner 的这个目录为空）
 # COPY os-checks/ /os-checks
 # WORKDIR /os-checks
-WORKDIR /check
-COPY ./os-checks .
-COPY --from=os-checker /check/test.json ./content/
-CMD ls -alh && echo $PWD && ls -alh / && npm install && npm run generate 
+# WORKDIR /check
+# COPY ./os-checks .
+# COPY --from=os-checker /check/test.json ./content/
+# CMD ls -alh && echo $PWD && ls -alh / && npm install && npm run generate 
     # cp -LR dist /check/
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Current dir: /.../ci
+git clone https://github.com/os-checker/ci.git
+cd ci
 CI_dir=$PWD
 echo CI_dir in docker is $CI_dir
 
@@ -9,22 +11,22 @@ cargo r --release
 
 # install latest os-checker
 echo "DEBUG=$DEBUG"
-if [[ "$DEBUG" == 1 ]]; then
-  echo 'compile os-checker from github source'
-  git clone https://github.com/os-checker/os-checker.git
-  cargo install --path os-checker
-  export RUST_LOG=debug
-else
-  echo 'download os-checker from github release'
-  curl -s https://api.github.com/repos/os-checker/os-checker/releases/latest |
-    grep browser_download_url |
-    grep os-checker-installer.sh |
-    cut -d '"' -f 4 |
-    wget -qi -
-  echo 'os-checker-installer.sh is downloaded'
-  sh os-checker-installer.sh
-fi
-os-checker --help
+# if [[ "$DEBUG" == 1 ]]; then
+#   echo 'compile os-checker from github source'
+#   git clone https://github.com/os-checker/os-checker.git
+#   cargo install --path os-checker
+#   export RUST_LOG=debug
+# else
+#   echo 'download os-checker from github release'
+#   curl -s https://api.github.com/repos/os-checker/os-checker/releases/latest |
+#     grep browser_download_url |
+#     grep os-checker-installer.sh |
+#     cut -d '"' -f 4 |
+#     wget -qi -
+#   echo 'os-checker-installer.sh is downloaded'
+#   sh os-checker-installer.sh
+# fi
+# os-checker --help
 
 # handle ansi via ansi2html or ansi2txt
 # apt install -y colorized-logs
@@ -39,8 +41,8 @@ os-checker --help
 # ansi2txt <summary.out >>summary.txt
 # echo '```' >>summary.txt
 
-ls -alh && echo PWD = $PWD
-os-checker --emit test.json # to be moved into ci/os-checks/content
+# ls -alh && echo PWD = $PWD
+# os-checker --emit test.json # to be moved into ci/os-checks/content
 
 # github pages dir
 # mkdir /check/.gh-pages
