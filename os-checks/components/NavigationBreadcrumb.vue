1011d695ae07a4fb8ba1a3c8c579fd7588f5f94f
zjp <jiping_zhou@foxmail.com>
2024-08-08 09:47:48 +0800

    chore(NavigationBreadcrumb): 使用 Button 和 Flex 布局而不是 Breadcrumb 组件

--- os-checks/components/NavigationBreadcrumb.vue
+++ os-checks/components/NavigationBreadcrumb.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <Breadcrumb :home="home" :model="items">
-      <template #item="{ item }">
-        <NuxtLink :to="item.url"><span :class="item.icon"></span></NuxtLink>
-      </template>
-      <template #separator> / </template>
-    </Breadcrumb>
+    <div class="naviBreadcrumb">
+      <Button icon="pi pi-home" text>
+        <NuxtLink to="/"><span class="pi pi-home" /></NuxtLink>
+      </Button>
+
+      <Button icon="pi pi-sitemap" text />
+    </div>
+
+    <!-- <Breadcrumb :home="home" :model="items"> -->
+    <!--   <template #item="{ item }"> -->
+    <!--     <NuxtLink :to="item.url"><span :class="item.icon" /></NuxtLink> -->
+    <!--   </template> -->
+    <!--   <template #separator> / </template> -->
+    <!-- </Breadcrumb> -->
   </div>
 </template>
 
 <script setup>
-const home = ref({ icon: 'pi pi-home', url: "/" });
-const items = ref([{ icon: 'pi pi-sitemap' }]);
+// const home = ref({ icon: 'pi pi-home', url: "/" });
+// const items = ref([{ icon: 'pi pi-sitemap' }]);
 </script>
+
+<style scoped>
+.naviBreadcrumb {
+  display: flex;
+  align-items: center;
+  /* 垂直居中所有子元素 */
+  height: 55px;
+  /* 容器的高度 */
+  --p-button-sm-padding-x: 0.8rem;
+}
+</style>
