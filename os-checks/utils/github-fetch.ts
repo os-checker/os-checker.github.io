01960996b3b54ae84de9b792a175b1b543fe36a9
zjp <jiping_zhou@foxmail.com>
2024-08-10 10:04:50 +0800

    fix: githubFetch url

--- os-checks/utils/github-fetch.ts
+++ os-checks/utils/github-fetch.ts
@@ -4,10 +4,11 @@ type Github = {
   path: string
 }
 
+// https://raw.githubusercontent.com/os-checker/os-checker.github.io/main/os-checks/public/test_raw_reports.json
 export default function (github: Github) {
   const repo = github.repo || "os-checker.github.io";
   const branch = github.branch || "main";
-  const url = `https://raw.githubusercontent.com/os-checker/${repo}/ui/${branch}/${github.path}`;
+  const url = `https://raw.githubusercontent.com/os-checker/${repo}/${branch}/${github.path}`;
   return useFetch(url, {
     // 构造简单请求来解决跨域问题（同时 github raw 资源支持所有源）
     "headers": { "content-type": "text/plain" },
