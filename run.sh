513fb3e42385e684fa34267209349ffc6e0e788c
zjp <jiping_zhou@foxmail.com>
2024-08-31 08:57:41 +0800

    chore(run.sh): clean up comments

--- run.sh
+++ run.sh
@@ -31,28 +31,9 @@ else
 fi
 os-checker --help
 
-# handle ansi via ansi2html or ansi2txt
-# apt install --silent -y colorized-logs
-
-# prepare os-checker configuration file
-# cp repos.yaml /check/
-# cd /check
-# Current dir: /check
-
-# os-checker 2>&1 >summary.out
-# echo '```text' >>summary.txt
-# ansi2txt <summary.out >>summary.txt
-# echo '```' >>summary.txt
-
-# ls -alh && echo PWD = $PWD
 os-checker --emit json --norun --setup # set up toolchains for all repos and checkers
-os-checker --emit /check/ui.json       # to be moved into ci/os-checks/content
-
 # /check 目录在 github action 和 Docker 容器之间共享
-
-# github pages dir
-# mkdir /check/.gh-pages
-# ansi2html <summary.out >/check/.gh-pages/index.html
+os-checker --emit /check/ui.json # to be moved into ci/os-checks/content
 
 # 构建网页应用，并从容器移至 Github Action 目录，待部署到 Github Pages
 cd os-checks && npm install && npm run generate && cp -LR dist /check/
