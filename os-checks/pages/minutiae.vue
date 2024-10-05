8a8f07a1896d0dc1727694af13b633e65bc1c6ce
zjp <jiping_zhou@foxmail.com>
2024-10-05 13:44:26 +0800

    feat(minutiae): 展示 Source
    https://github.com/os-checker/os-checker/pull/119 给前端生成了 Source 数据，初步展示
    
    close https://github.com/os-checker/os-checker.github.io/issues/81

--- os-checks/pages/minutiae.vue
+++ os-checks/pages/minutiae.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <DataTable :value="resolved" paginator :rows="10" :rowsPerPageOptions="[10, 20, 50]" sortMode="multiple" scrollable
-      scrollHeight="700px">
+      scrollHeight="400px">
       <Column v-for="col of resolvedColumns" :key="col.field" :field="col.field" :header="col.header" sortable />
+    </DataTable>
+
+    <DataTable :value="sources" paginator :rows="10" :rowsPerPageOptions="[10, 20, 50]" sortMode="multiple" scrollable
+      scrollHeight="400px">
+      <Column v-for="col of sourcesColumns" :key="col.field" :field="col.field" :header="col.header" sortable />
     </DataTable>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { type Resolved } from '~/shared/types';
+import { type Resolved, type Source } from '~/shared/types';
 
 const resolved = ref<Resolved[]>([]);
 
@@ -22,4 +27,18 @@ const resolvedColumns = [
 
 githubFetch<Resolved[]>({ path: "ui/targets/AsyncModules/embassy-priority/resolved.json" })
   .then(data => resolved.value = data);
+
+const sources = ref<Source[]>([]);
+
+const sourcesColumns = [
+  { field: "pkg", header: "Pkg" },
+  { field: "target", header: "Target" },
+  { field: "src", header: "Src" },
+  { field: "path", header: "Path" },
+  { field: "used", header: "Used" },
+  { field: "specified", header: "Specified" },
+]
+
+githubFetch<Source[]>({ path: "ui/targets/AsyncModules/embassy-priority/sources.json" })
+  .then(data => sources.value = data);
 </script>
