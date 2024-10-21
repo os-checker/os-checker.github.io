51d10838d50ad8e2b53474e901acb269b8652ba4
zjp <jiping_zhou@foxmail.com>
2024-10-22 07:27:48 +0800

    chore(nuxt): baseURL: '/os-checker/'

--- os-checks/nuxt.config.ts
+++ os-checks/nuxt.config.ts
@@ -4,7 +4,7 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-08-28',
   app: {
-    // baseURL: '/ci/',
+    // baseURL: '/os-checker/',
     head: { link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }] }
   },
   devtools: {
