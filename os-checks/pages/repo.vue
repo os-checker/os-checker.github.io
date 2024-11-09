1e1b5b351b66ee342062b564641ca0435d209b9e
zjp <jiping_zhou@foxmail.com>
2024-11-09 12:21:48 +0800

    feat(repo): add repo page; define Info

---
+++ os-checks/pages/repo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <DataTable :value="data" scrollable :scrollHeight="tableHeight" showGridlines selectionMode="single" -->
+    <!--   v-model:selection="selectedPkg" v-model:filters="selected.text" -->
+    <!--   :globalFilterFields="['user', 'repo', 'pkg', 'description', 'categories']" removableSort sortMode="multiple" -->
+    <!--   paginator :rows="10" :rowsPerPageOptions="[5, 10, 20, 50, 100, 200, 1000]"> -->
+    <!---->
+    <!-- </DataTable> -->
+    {{ summaries }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Info } from '~/shared/repo';
+
+const summaries = ref<Info[]>([]);
+
+githubFetch<Info[]>({
+  path: "plugin/github-api/info/summaries.json"
+}).then(val => {
+  summaries.value = val;
+});
+
+</script>
