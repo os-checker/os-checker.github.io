965b10200301dc64fd10b27dcfe087e60b1d6faf
zjp <jiping_zhou@foxmail.com>
2024-11-09 14:44:25 +0800

    feat: define fmtDateTime

---
+++ os-checks/utils/fmt-date-time.ts
@@ -0,0 +1,15 @@
+
+export default function (utc: string | null) {
+  if (!utc) { return "null"; }
+
+  const date = new Date(utc);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 月份从0开始，所以加1
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
