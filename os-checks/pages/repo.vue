c983b5b22a24cfac53320ed899e81bd6ef9a722b
zjp <jiping_zhou@foxmail.com>
2024-11-09 14:44:01 +0800

    feat(repo): add more columns

--- os-checks/pages/repo.vue
+++ os-checks/pages/repo.vue
@@ -6,8 +6,8 @@
     <!--   paginator :rows="10" :rowsPerPageOptions="[5, 10, 20, 50, 100, 200, 1000]"> -->
     <!---->
     <!-- </DataTable> -->
-    <DataTable :value="repo" scrollable :scrollHeight="tableHeight" showGridlines selectionMode="single" paginator
-      :rows="10" :rowsPerPageOptions="[5, 10, 20, 50, 100, 200, 1000]">
+    <DataTable :value="repo" scrollable :scrollHeight="tableHeight" showGridlines selectionMode="single" removableSort
+      sortMode="multiple" paginator :rows="10" :rowsPerPageOptions="[5, 10, 20, 50, 100, 200, 1000]">
 
       <Column frozen sortable field="idx" header="Idx" />
       <Column frozen sortable field="user" header="User" style="min-width: 150px;" />
@@ -20,7 +20,23 @@
         </template>
       </Column>
 
-      <Column sortable field="license" header="License" :pt="ptColumn" />
+      <Column sortable field="license" header="License" :pt="ptColumnCenter" />
+
+      <Column sortable field="homepage" header="Home" :pt="ptColumnCenter">
+        <template #body="{ data }">
+          <NuxtLink v-if="data.homepage" :to="data.homepage" target="_blank" class="nav-link">
+            <Button icon="pi pi-external-link" link />
+          </NuxtLink>
+        </template>
+      </Column>
+
+      <Column sortable field="description" header="Description" style="max-width: 500px; min-width: 300px" />
+      <Column sortable field="created_at" header="Created" :pt="ptColumnCenter" />
+      <Column sortable field="pushed_at" header="Updated" :pt="ptColumnCenter" />
+      <Column sortable field="active_days" header="Active Days" :pt="ptColumnRight" />
+      <Column sortable field="size" header="Size" :pt="ptColumnRight" />
+      <Column sortable field="contributions" header="Contri-butions" :pt="ptColumnRight" />
+      <Column sortable field="contributors" header="Contri-butors" :pt="ptColumnRight" />
 
     </DataTable>
   </div>
@@ -40,9 +56,13 @@ onMounted(() => {
 });
 
 // styling
-const ptColumn = ref({
+const ptColumnCenter = ref({
   columnHeaderContent: { style: { "justify-content": "center" } },
   bodyCell: { style: { "text-align": "center" } }
+});
+const ptColumnRight = ref({
+  columnHeaderContent: { style: { "justify-content": "right" } },
+  bodyCell: { style: { "text-align": "right" } }
 });
 
 const summaries = ref<Output[]>([]);
