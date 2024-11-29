f65dc4a13ce0bb2ad10aafaac72bda6f935145f3
zjp <jiping_zhou@foxmail.com>
2024-11-29 14:06:11 +0800

    feat: Redirect /info and queries to root

---
+++ os-checks/pages/info.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: [
+    function (to, _from) {
+      // Redirect /info and queries to root
+      if (to.path === "/info") {
+        return navigateTo({ path: "/", query: to.query });
+      }
+    }
+  ]
+})
+</script>
