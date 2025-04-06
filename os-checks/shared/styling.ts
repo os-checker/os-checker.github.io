f6a5c39162ac6f1da03da92ecfd61c765c73f1ff
zjp <jiping_zhou@foxmail.com>
2025-04-06 20:39:24 +0800

    chore: mv ptColumn{Center,Right} to styling mod and renaming

---
+++ os-checks/shared/styling.ts
@@ -0,0 +1,15 @@
+/**
+ * Make header and column content center aligend.
+ */
+export const center = {
+  columnHeaderContent: { style: { "justify-content": "center" } },
+  bodyCell: { style: { "text-align": "center" } }
+};
+
+/**
+ * Make header and column content right aligend.
+ */
+export const right = ref({
+  columnHeaderContent: { style: { "justify-content": "right" } },
+  bodyCell: { style: { "text-align": "right" } }
+});
