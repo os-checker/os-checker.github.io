a5a8fec569cdd125eb65551a1872c70c5872bd0f
zjp <jiping_zhou@foxmail.com>
2025-04-06 21:39:47 +0800

    css(diagnostics): make number cells right aligned
    close https://github.com/os-checker/os-checker.github.io/issues/160
    
    NOTE: the bodyCellContent trick for TreeTable as shown in the link.

--- os-checks/shared/styling.ts
+++ os-checks/shared/styling.ts
@@ -1,5 +1,5 @@
 /**
- * Make header and column content center aligend.
+ * Make header and column content center aligned.
  */
 export const center = {
   columnHeaderContent: { style: { "justify-content": "center" } },
@@ -7,9 +7,20 @@ export const center = {
 };
 
 /**
+ * Make header and column content right aligned.
+ */
+export const right = {
+  columnHeaderContent: { style: { "justify-content": "right" } },
+  bodyCell: { style: { "text-align": "right" } },
+};
+
+/**
  * Make header and column content right aligend.
  */
-export const right = ref({
+export const rightCell = {
   columnHeaderContent: { style: { "justify-content": "right" } },
-  bodyCell: { style: { "text-align": "right" } }
-});
+  bodyCell: { style: { "text-align": "right" } },
+  // TreeTable need this to invalidate .p-treetable-body-cell-content { display: flex; }
+  // to make right alignment work.
+  bodyCellContent: { style: { display: 'block' } }
+};
